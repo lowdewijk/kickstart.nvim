aecbf1637258dbbfa895fa903fecb659a4535737
lowdewijk <lodewijk.bogaards@gmail.com>
2024-07-18 17:37:16 +0200

    add lsp_signature so we get parameter documentation when typing a function

--- init.lua
+++ init.lua
@@ -900,6 +900,14 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>e', '<CMD>Neotree toggle<CR>', { desc = '[E]xplore tree' })
     end,
   },
+  {
+    'ray-x/lsp_signature.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    config = function(_, opts)
+      require('lsp_signature').setup(opts)
+    end,
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
